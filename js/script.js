99389b481e83a433d5e998b9e10bae7f4c787029
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-07 13:25:18 +0200

    scrittura codice per richiesta dati e rapresentazione in html dei dati raccolti

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// richiesta nome utente
+const userName = prompt ("Il tuo nome");
+
+// richiesta cognome utente
+const userSecondName = prompt ("Il tuo cognome");
+
+// richiesta cognome utente
+const preferColor = prompt ("Il tuo colore preferito");
+
+// richiesta cognome utente
+const userData = 24;
+
+// scrittura in html su specifico ID dei dati acquisiti
+document.getElementById('output-generetor').innerHTML = userName + userSecondName + preferColor + userData;
